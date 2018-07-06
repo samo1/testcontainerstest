15207b95d0540c32f748f731061a01d905e6c5a3
Samuel Valis <samuel.valis@meluzina.sk>
2018-07-07 00:10:39 +0200

    small refactor

--- src/test/java/sk/valiss/test/OracleContainerTest.java
+++ src/test/java/sk/valiss/test/OracleContainerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import com.github.dockerjava.api.command.CreateContainerCmd;
@@ -15,12 +16,13 @@ public class OracleContainerTest {
 
     private static final Logger log = LoggerFactory.getLogger(OracleContainerTest.class);
     private static final long ONE_GB = 1024L * 1024L * 1024L;
-    private static Consumer<CreateContainerCmd> shmSizeModifier = cmd -> cmd.getHostConfig().withShmSize(ONE_GB);
+    private static final Consumer<CreateContainerCmd> shmSizeModifier = cmd -> cmd.getHostConfig().withShmSize(ONE_GB);
+    private static final Consumer<OutputFrame> logConsumer = new Slf4jLogConsumer(log);
 
     @ClassRule
     public static OracleContainer oracle = (OracleContainer) new OracleContainer("oracle/database:11.2.0.2-xe")
             .withCreateContainerCmdModifier(shmSizeModifier)
-            .withLogConsumer(new Slf4jLogConsumer(log));
+            .withLogConsumer(logConsumer);
 
     @Test
     public void test1() {
