6ea349cff7a48891ee7fa3654d6ef7dba10813ab
Samuel Valis <samuel.valis@meluzina.sk>
2018-07-07 21:41:34 +0200

    added mssql

--- src/test/java/sk/valiss/test/TestContainerTest.java
+++ src/test/java/sk/valiss/test/TestContainerTest.java
@@ -6,15 +6,16 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.MSSQLServerContainer;
 import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 
 import com.github.dockerjava.api.command.CreateContainerCmd;
 
-public class OracleContainerTest {
+public class TestContainerTest {
 
-    private static final Logger log = LoggerFactory.getLogger(OracleContainerTest.class);
+    private static final Logger log = LoggerFactory.getLogger(TestContainerTest.class);
     private static final long ONE_GB = 1024L * 1024L * 1024L;
     private static final Consumer<CreateContainerCmd> shmSizeModifier = cmd -> cmd.getHostConfig().withShmSize(ONE_GB);
     private static final Consumer<OutputFrame> logConsumer = new Slf4jLogConsumer(log);
@@ -22,6 +23,10 @@ public class OracleContainerTest {
     @ClassRule
     public static OracleContainer oracle = (OracleContainer) new OracleContainer("oracle/database:11.2.0.2-xe")
             .withCreateContainerCmdModifier(shmSizeModifier)
+            .withLogConsumer(logConsumer);
+
+    @ClassRule
+    public static MSSQLServerContainer mssql = (MSSQLServerContainer) new MSSQLServerContainer("microsoft/mssql-server-linux:2017-GDR")
             .withLogConsumer(logConsumer);
 
     @Test
